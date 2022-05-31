8126e2fe12b8479fe0ac9a6d83fe5b56987037c2
Glitch (sebastian-kaczynski---client-project---movie-info) <none>
2022-05-31 22:23:07 +0000

    😜🥤 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -23,10 +23,12 @@ $('h1').click(function(){
 //     });
 // });
 let apiMovieUrl = `https://api.themoviedb.org/3/discover/movie?api_key=ba641dd317ef0ebb223f092b37068838&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate`
+let RanGenNum = 0
 
 $("button").click(function() {
   let SearchedMovie = $("input").val();
-
+  $(".results").empty()
+  
 let SearchEngine = `https://api.themoviedb.org/3/search/movie?api_key=ba641dd317ef0ebb223f092b37068838&query=${SearchedMovie}`  
 
 fetch(SearchEngine)
@@ -34,10 +36,58 @@ fetch(SearchEngine)
     return response.json();
 })
   .then(function(data) {
-    console.log(data);
-  
+    data.results.forEach((element, index) =>                  
+    {
+    console.log(element);
+    let popularityScoreClass = "popularScore";
+      if(element.popularity > 90){
+        popularityScoreClass = "popularScore"
+      }
+      if(element.popularity > 50 && element.popularity< 90){
+        popularityScoreClass = "semipopularScore"
+      }
+      if(element.popularity < 50){
+        popularityScoreClass = "unpopularScore"
+      }
+    
+    let output = `
+                  <ul>
+                  <div class="movieInfo">
+                  <hr>
+                  <div class="title"><b>${element.original_title}</b></div>
+                  <div class="info">
+                  <b>Description:</b> ${element.overview}
+                  <br>
+                  <b>Language:</b> ${element.original_language}
+                  <br>
+                  <b>Year of Release:</b> ${element.release_date}
+                  <br>
+          
+                  <div class="resultEntry"><b>Popularity Score:</b> <div class="${popularityScoreClass}">${element.popularity}</div></div>
+                  </div>
+                  <hr>
+                  </div>
+                  <br>
+                  </br>
+                  </ul>
+                  
+                  `
+    if(element.original_language === "en") {
+      
+    }
+      
+
+      
+      
+    $(".results").append(output)
+      
+        
+    })
+    
+  })
+
 });
-});
+
 
 
 
